ebec66602ed712e5edf30067c0bf379f3dfd8f21
GustavoSantos16 <gustavo.santos@ddrtech.com.br>
2020-07-28 00:23:40 -0300

    Back button

--- src/app/visualizar/visualizar.page.ts
+++ src/app/visualizar/visualizar.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-visualizar',
@@ -7,9 +8,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VisualizarPage implements OnInit {
 
-  constructor() { }
+  constructor(private alertController: AlertController) { }
 
   ngOnInit() {
+  }
+
+  async presentAlertConfirm() {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'Deseja excluir?',
+      message: 'Tem certeza de que deseja excluir?',
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: (blah) => {
+            console.log('Confirm Cancel: blah');
+          }
+        }, {
+          text: 'Sim, excluir',
+          handler: () => {
+            console.log('Confirm Okay');
+          }
+        }
+      ]
+    });
+
+    await alert.present();
   }
 
 }
