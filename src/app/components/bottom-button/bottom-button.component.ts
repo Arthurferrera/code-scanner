6eae1768c04f509eba7c841faa7887b5d7dedeb9
GustavoSantos16 <gustavo.santos@ddrtech.com.br>
2020-07-29 23:21:54 -0300

    Barcode scanner in button

--- src/app/components/bottom-button/bottom-button.component.ts
+++ src/app/components/bottom-button/bottom-button.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
 
 @Component({
   selector: 'app-bottom-button',
@@ -8,14 +9,54 @@ import { NavController } from '@ionic/angular';
 })
 export class BottomButtonComponent implements OnInit {
 
-  constructor(private navCtrl: NavController) { }
+  public id_codigo: number;
+  public dataFormatada: string;
+  public data: any = {};
+  listaHistorico: any = [];
+
+  constructor(private barcodeScanner: BarcodeScanner, private navCtrl: NavController) { }
 
   ngOnInit() {
 
   }
 
   read() {
-    this.navCtrl.navigateRoot('home');
-  }
 
+    this.listaHistorico = JSON.parse(localStorage.getItem('scan.history'));
+    const dataAtual = new Date();
+    const diaSplit = dataAtual.getDate();
+    const dia = (diaSplit < 10) ? `0${diaSplit}` : diaSplit;
+    const mesSplit = dataAtual.getMonth() + 1;
+    const mes = (mesSplit < 10) ? `0${mesSplit}` : mesSplit;
+    const ano = dataAtual.getFullYear();
+    this.dataFormatada = `${ano}-${mes}-${dia}`;
+
+    if (this.listaHistorico) {
+      this.id_codigo = this.listaHistorico.length + 1;
+    } else {
+      this.id_codigo = 1;
+      this.listaHistorico = [];
+    }
+
+
+    //Ler QRCODE
+    this.barcodeScanner.scan().then(barcodeData => {
+      console.log('Barcode data', barcodeData);
+
+      this.data = {
+        id: this.id_codigo,
+        title: '',
+        text: barcodeData.text,
+        date: this.dataFormatada
+      };
+
+      this.listaHistorico.push(this.data);
+
+      localStorage.setItem('scan.history', JSON.stringify(this.listaHistorico));
+
+      this.navCtrl.navigateRoot('historico');
+    }).catch(err => {
+      console.log('Error', err);
+    });
+  }
 }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
-import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -9,54 +7,10 @@ import { NavController } from '@ionic/angular';
 })
 export class HomePage implements OnInit {
 
-  public id_codigo: number;
-  public dataFormatada: string;
-  public data: any = {};
-  listaHistorico: any = [];
-
-
-
-  constructor(private barcodeScanner: BarcodeScanner, private navCtrl: NavController) { }
+  constructor() { }
 
 
   ngOnInit() {
-
-    this.listaHistorico = JSON.parse(localStorage.getItem('scan.history'));
-    const dataAtual = new Date();
-    const diaSplit = dataAtual.getDate();
-    const dia = (diaSplit < 10) ? `0${diaSplit}` : diaSplit;
-    const mesSplit = dataAtual.getMonth() + 1;
-    const mes = (mesSplit < 10) ? `0${mesSplit}` : mesSplit;
-    const ano = dataAtual.getFullYear();
-    this.dataFormatada = `${ano}-${mes}-${dia}`;
-
-    if (this.listaHistorico) {
-      this.id_codigo = this.listaHistorico.length + 1;
-    } else {
-      this.id_codigo = 1;
-      this.listaHistorico = [];
-    }
-
-
-    //Ler QRCODE
-    this.barcodeScanner.scan().then(barcodeData => {
-      console.log('Barcode data', barcodeData);
-
-      this.data = {
-        id: this.id_codigo,
-        title: '',
-        text: barcodeData.text,
-        date: this.dataFormatada
-      };
-
-      this.listaHistorico.push(this.data);
-
-      localStorage.setItem('scan.history', JSON.stringify(this.listaHistorico));
-
-      this.navCtrl.navigateRoot('historico');
-    }).catch(err => {
-      console.log('Error', err);
-    });
   }
 
 }
